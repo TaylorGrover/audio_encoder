8b50576fe2d379197e38d947193a87989e812974
Taylor Grover <tgrover@cc.nmt.edu>
2022-11-14 00:09:49 -0700

    Conv to wav test

--- audio_test.py
+++ audio_test.py
@@ -2,6 +2,7 @@ import librosa
 import os
 import pathlib
 import numpy as np
+import soundfile as sf
 
 root_dir = "mswc_microset/"
 
@@ -15,4 +16,6 @@ def get_random_file():
     return np.random.choice(get_all_files())
 
 rand_file = get_random_file()
+print(rand_file)
 X, sr = librosa.load(rand_file)
+sf.write("test.wav", X, sr, format="ogg")
